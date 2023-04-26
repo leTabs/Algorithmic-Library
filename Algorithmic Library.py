7e750bfb9e2634daef356b64a64150f4c1d5598a
leTabs <125308756+leTabs@users.noreply.github.com>
2023-04-26 22:03:05 +0300

    Update Algorithmic Library.py

--- Algorithmic Library.py
+++ Algorithmic Library.py
@@ -1,22 +1,11 @@
 # -- ALGORITHMIC LIBRARY--
+import sys
 alg_types =[
 'Searching',
 'Sorting'
 ]
+
 index = 1
-
-def search():
-    from Searching_algorithms import all_searchings
-    return  all_searchings()
-def sorting():
-    print('here will be the sortings')
-
-alg_imports =[
-    search,
-    sorting
-]
-
-
 print('Types of Algorithms')
 print('-'*40)
 for type in alg_types:
@@ -24,9 +13,25 @@ for type in alg_types:
     index += 1
 print('-'*40)
 
+def search():
+    from Searching_algorithms import all_searchings
+    return  all_searchings()
+def sorting():
+    from Sorting_algorithms import all_sortings
+    return all_sortings()
+    
+alg_imports =[
+    search, 
+    sorting
+]
 #----------------------------------------------
 type_choice = int(input('Number: ')) - 1
 print()
 print(f'The {alg_types[type_choice]} are:')
 print()
 alg_imports[type_choice]()
+
+# remeber to add searching in the searchin algorithms .py
+
+close = input(':')
+sys.exit()
