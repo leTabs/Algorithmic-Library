c7d53398813c445c77138c36a352a15d247fee46
leTabs <125308756+leTabs@users.noreply.github.com>
2023-05-01 14:44:33 +0300

    Update Graph_algorithms.py

--- Graph_algorithms.py
+++ Graph_algorithms.py
@@ -2,6 +2,7 @@ import ast
 graphings = [
     'Dijkstra\'s',
     'Bellman Ford',
+    'Floyd Warshall'
 ]
 def dijkstra(graph, start):
     from graphs.dijkstras import dijkstra
@@ -9,9 +10,13 @@ def dijkstra(graph, start):
 def bellman_ford(graph, start):
     from graphs.bellman_ford import bellman_ford
     print(bellman_ford(graph, start))
+def floyd_warshall(graph):
+   from graphs.floyd_warshall import floyd_warshall
+   print(floyd_warshall)
 graph_imports = [
     dijkstra,
-    bellman_ford
+    bellman_ford,
+    floyd_warshall
 ]
 
 def all_graphings():
